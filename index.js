1ce1284bb75c77a5edc6fd44c45a9058637da5db
Dharam Kumar <iamdharam21@gmail.com>
2022-06-12 00:28:48 +0530

    Day1 Task

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import EmployeeRoute from './routes/EmployeeRoute.js'
+import env from 'dotenv';
+env.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/employee', EmployeeRoute);
+
+app.listen(PORT, ()=>{
+    console.log(`Server : http://localhost:${PORT}`);
+});
